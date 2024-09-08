d2e4cfc4f46c59884fe84f8453c29ac46ccd020a
Aniket Sharma <89375574+aniketsham@users.noreply.github.com>
2024-09-08 15:29:27 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser";
 
 const app=express()
 app.use(cors({
-    origin:["https://user-manager-digitalsalt-task.netlify.app","https://digital-task-frontend-35rn8m34t-aniketshams-projects.vercel.app"],
+    origin:["https://digital-task-frontend-35rn8m34t-aniketshams-projects.vercel.app"],
     methods:["GET","POST","PUT","DELETE"],
     credentials:true,
 }))
