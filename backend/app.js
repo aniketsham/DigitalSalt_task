ab8cc1ec390f1416eebda1d3df030f7bc1520677
Aniket Sharma <89375574+aniketsham@users.noreply.github.com>
2024-09-08 13:48:02 +0530

    updating cors

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser";
 
 const app=express()
 app.use(cors({
-    origin:["http://localhost:3000"],
+    origin:["https://user-manager-digitalsalt-task.netlify.app"],
     methods:["GET","POST","PUT","DELETE"],
     credentials:true,
 }))
