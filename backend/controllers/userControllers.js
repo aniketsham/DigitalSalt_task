e4e7edb6cfbaff70be476b60ab04b183f31ac695
Aniket Sharma <89375574+aniketsham@users.noreply.github.com>
2024-09-08 15:30:22 +0530

    Update userControllers.js

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -193,10 +193,12 @@ const sendToken=(user,statusCode,res,message)=>{
     const token=user.getJWTToken();
     const options={
         expires:new Date(
-            Date.now()+ (10 * 24 * 60 * 60 * 1000)
+            Date.now()+ (7 * 24 * 60 * 60 * 1000)
         ),
-        httpOnly:true,
-        secure:true
+        httpOnly: true,
+        secure: true,
+      sameSite: "None",
+        
         
     };
     res.status(statusCode).cookie("token",token,options).json({
